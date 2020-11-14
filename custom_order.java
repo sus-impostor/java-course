d3f943ba64a073348e6d7bfcad868087916b2c49
sus-impostor <73416081+sus-impostor@users.noreply.github.com>
2020-11-14 21:09:03 +0700

    Update custom_order.java

--- custom_order.java
+++ custom_order.java
@@ -1,2 +1,13 @@
-javac custom_order.java
-java custom_order
+import java.util.Scanner;
+public class MyClass {
+  public static int temperatureConvert(double fahrenheit) {
+    double celsius = fahrenheit * 1.8 + 32; // temperature conversion
+    return celsius;
+  }
+  public static void main(String[] args) {
+    Scanner myObj = new.Scanner(System.in);
+    System.out.println("Temperature in Fahrenheit:");
+    
+    double f = myObj.nextLine();
+    System.out.println(temperatureConvert(f));
+}
